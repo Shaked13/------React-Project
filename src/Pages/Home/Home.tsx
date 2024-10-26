55dd1accc7e8a5f2d4b6a538339f2ad6d2241c85
Shaked13 <shaked1234538@gmail.com>
2024-10-26 22:40:50 +0300

    sign in

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -97,7 +97,6 @@ const Home = () => {
                             onPageChange={onPageChange}
                             showIcons
                         />
-
                     )}
                 </div>
             </div>
